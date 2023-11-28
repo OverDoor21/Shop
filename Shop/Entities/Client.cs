52f4f319b253574ef7aa7c426082ab0db0188be0
OverDoor21 <aulin.ivan32@gmail.com>
2023-11-28 17:09:42 +0100

    Add project files.

---
+++ Shop/Entities/Client.cs
@@ -0,0 +1,25 @@
+﻿namespace Shop.Entities
+{
+    public class Client : User
+    {
+        public void AcceptOffer(int OfferID)
+        {
+
+        }
+
+        public void CreateBargain()
+        {
+
+        }
+
+        public int ChangePrice(int price)
+        {
+            return price;
+        }
+
+        public void BuyProduct()
+        {
+
+        }
+    }
+}
---
+++ Shop/Entities/Seller.cs
@@ -0,0 +1,25 @@
+﻿namespace Shop.Entities
+{
+    public class Seller : User
+    {
+        public void CreateLot()
+        {
+
+        }
+
+        public void DeleteLot(int lotId)
+        {
+
+        }
+
+        public void AcceptBaragin()
+        {
+
+        }
+
+        public void DeclainBaragin()
+        {
+
+        }
+    }
+}
